2b4813ea1d897d314a1457a72f0853fe8fde71f7
Artur Nascimento <artur.nascimento@outlook.com>
2021-06-29 15:28:29 -0300

    DioAPIMongoDB

---
+++ ApiDioMongoDB/Data/Collections/Infectado.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+
+
+namespace ApiDioMongoDB.Data.Collections
+{
+    public class Infectado
+    {
+        public Infectado(DateTime dataNascimeto, string sexo, double latitude, double longetude)
+        {
+            this.DataNascimento = dataNascimeto;
+            this.Sexo = sexo;
+            this.Localizacao = new GeoJson2DGeographicCoordinates(longetude, latitude);
+        }
+
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+    }
+
+    
+
+}
